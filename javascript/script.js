85663c72d5055125ac4aff5d6eccfe98a4949327
Dat Tran <127409761+drxaviert@users.noreply.github.com>
2026-06-15 22:39:49 -0400

    Refactor rps logic

--- javascript/script.js
+++ javascript/script.js
@@ -1,90 +1,80 @@
-// ***Get computer choice
+function getPlayerChoice() {
+  let playerChoice = prompt("What's your answer?");
+  return (playerChoice =
+    playerChoice.at(0).toUpperCase() + playerChoice.slice(1).toLowerCase());
+}
+
 function getComputerChoice() {
-  let randNo = Math.floor(Math.random() * 3);
-  let computerChoice;
+  let rand = Math.floor(Math.random() * 3);
 
-  if (randNo >= 2) {
-    computerChoice = "Rock";
-  } else if (randNo >= 1) {
-    computerChoice = "Paper";
+  if (rand >= 2) {
+    return "Rock";
+  } else if (rand >= 1) {
+    return "Paper";
   } else {
-    computerChoice = "Scissor";
+    return "Scissor";
   }
-
-  return computerChoice;
 }
 
-// ***Get human choice
-function getHumanChoice() {
-  let humanChoice = prompt("What's your answer?");
+function playRound(playerChoice, computerChoice) {
+  const a = playerChoice;
+  const b = computerChoice();
 
-  // Make humanChoice case insensitive
-  humanChoice =
-    humanChoice.at(0).toUpperCase() + humanChoice.slice(1).toLowerCase();
-
-  return humanChoice;
-}
-
-// ***Set initial score
-let humanScore, computerScore;
-
-humanScore = 0;
-computerScore = 0;
-
-// ***Game's logic
-let message, humanSelection, computerSelection;
-
-const winMessage = "You win!";
-const loseMessage = "You lose!";
-const drawMessage = "It's a draw!";
-
-function playRound(humanChoice, computerChoice) {
-  if (humanChoice == computerChoice) {
-    message = drawMessage;
-  } else if (humanChoice === "Rock" && computerChoice === "Scissor") {
-    message = winMessage;
-  } else if (humanChoice === "Paper" && computerChoice === "Rock") {
-    message = winMessage;
-  } else if (humanChoice === "Scissor" && computerChoice === "Paper") {
-    message = winMessage;
+  if (a === b) {
+    return "It's a draw!";
+  } else if (
+    (a === "Rock" && b === "Scissor") ||
+    (a === "Scissor" && b === "Paper") ||
+    (a === "Paper" && b === "Rock")
+  ) {
+    return "Player wins!";
+    playerScore++;
   } else {
-    message = loseMessage;
-  }
-
-  humanSelection = humanChoice;
-  computerSelection = computerChoice;
-  return message;
-}
-
-function playGame(numberOfMatches) {
-  for (let i = 0; i < numberOfMatches; i++) {
-    let oneRound = playRound(getHumanChoice(), getComputerChoice());
-
-    if (message === winMessage) {
-      humanScore++;
-    } else if (message === loseMessage) {
-      computerScore++;
-    }
-
-    console.log(
-      `${oneRound} Your choice: ${humanSelection} | Computer choice: ${computerSelection}`,
-    );
-
-    // console.log(humanScore);
-    // console.log(computerScore);
-  }
-
-  console.log(
-    `Total | Your score: ${humanScore} | Computer score: ${computerScore}`,
-  );
-
-  if (humanScore > computerScore) {
-    console.log(`Congratulations! You have won `);
-  } else if (humanScore < computerScore) {
-    console.log(`Tough luck! The computer has won`);
-  } else {
-    console.log(`Eh, it's a  tie`);
+    return "Computer wins!";
+    computerScore++;
   }
 }
 
-playGame(3);
+const resultContainer = document.querySelector("#resultContainer");
+
+const btnRock = document.querySelector("#btnRock");
+btnRock.addEventListener("click", (event) => {
+  let playerChoice = "Rock";
+  let resultFromGame = playRound(playerChoice, getComputerChoice);
+  console.log(resultFromGame);
+
+  const result = document.createElement("div");
+  result.textContent = resultFromGame;
+  resultContainer.appendChild(result);
+});
+
+const btnPaper = document.querySelector("#btnPaper");
+btnPaper.addEventListener("click", (event) => {
+  let playerChoice = "Paper";
+  let resultFromGame = playRound(playerChoice, getComputerChoice);
+  console.log(resultFromGame);
+
+  const result = document.createElement("div");
+  result.textContent = resultFromGame;
+  resultContainer.appendChild(result);
+});
+
+const btnScissor = document.querySelector("#btnScissor");
+btnScissor.addEventListener("click", (event) => {
+  let playerChoice = "Scissor";
+  let resultFromGame = playRound(playerChoice, getComputerChoice);
+  console.log(resultFromGame);
+
+  const result = document.createElement("div");
+  result.textContent = resultFromGame;
+  resultContainer.appendChild(result);
+});
+
+// let playerScore = 0;
+// let computerScore = 0;
+
+// if (playerScore === 5) {
+//   console.log("We've got a winner! It's the player");
+// } else if (computerScore === 5) {
+//   console.log("We've got a winner! It's the computer");
+// }
